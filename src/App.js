3c3f6c8b96e10b93ec786a29c8f3e47729a42e57
jzliu <jzliu@uwaterloo.ca>
2024-05-04 00:38:55 -0400

    make sure that you stay on current mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const current_theme = localStorage.getItem("current_state");
+  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+
+  useEffect(() => {
+    localStorage.setItem("current_state", theme);
+  });
 
   return (
     <div className={`container ${theme}`}>
