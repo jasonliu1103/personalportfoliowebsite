fe2e708114bdb0a6e2efe96c91661ece931766d4
jzliu <jzliu@uwaterloo.ca>
2024-05-06 22:45:27 -0400

    Added typeanimation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
-import resume from '../assets/JasonLiuResume.pdf';
+import resume from "../assets/JasonLiuResume.pdf";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = ({ theme }) => {
   return (
     <div>
       <h1
-        className="toggle-heading"
+        className="title"
         style={{
           color: theme === "light" ? "#000" : "#fff",
         }}
       >
         Welcome! My name is Jason.
       </h1>
+      <TypeAnimation
+        className="typeanimation"
+        style={{
+          color: theme === "light" ? "#000" : "#fff",
+        }}
+        sequence={[
+          "I'm an aspiring engineer",
+          1500,
+          "I'm a music enthusiast",
+          1500,
+          "I'm an ECE student at UWaterloo",
+          1500,
+        ]}
+        wrapper="span"
+        speed={200}
+        deletionSpeed={200}
+        repeat={Infinity}
+        cursor={true}
+        key={theme}
+      />
       <p
-        className="toggle-heading"
+        className="smaller-heading"
         style={{
           color: theme === "light" ? "#000" : "#fff",
         }}
@@ -22,11 +43,7 @@ const Home = ({ theme }) => {
         This is my website. You will find everything you need to know about me
         here. Enjoy.
       </p>
-      <a
-        href={resume}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href={resume} target="_blank" rel="noopener noreferrer">
         Resume
       </a>
       <a
