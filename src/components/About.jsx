6e74ecb744901ad3ec1c1fd75fd249af3e6b3d45
jzliu <jzliu@uwaterloo.ca>
2024-05-08 21:01:32 -0400

    Added sticky navbar, navbar blurs when scrolls, styled about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,71 @@
 import React from "react";
+import "./About.css";
 
-const About = () => {
+const About = ({ theme }) => {
   return (
     <section id="about">
-      <div></div>
+      <div className="About">
+        <h1
+          className="title"
+          style={{ color: theme === "light" ? "#000" : "#fff" }}
+        >
+          About Me
+        </h1>
+        <p
+          className="smaller-heading1"
+          style={{
+            color: theme === "light" ? "#000" : "#fff",
+          }}
+        >
+          My name is Jason, I am a{" "}
+          <b>
+            second-year computer engineering student at the University of
+            Waterloo.
+          </b>{" "}
+          I have a keen sense of detail, I enjoy problem-solving and I am a fast
+          learner. I have much experience dealing with IT-related issues from
+          my previous co-op terms.
+        </p>
+        <p
+          className="smaller-heading2"
+          style={{
+            color: theme === "light" ? "#000" : "#fff",
+          }}
+        >
+          I previously worked for the Ontario Ministry of Education and each day
+          brought a new set of issues to resolve for end users ranging from
+          login issues, and issues involving access to different roles within
+          EFIS(Education Financial Information System). I was also responsible
+          for front-end development work which involved converting a large user
+          guide into a webpage. During my first co-op term, I had to communicate
+          with brokers to resolve client issues such as declined/missing credit
+          card information or missing files. I would then process insurance
+          policies, renewals, and cancellations for the insurance companies
+          through their portals. My time as a youth carrier reinforced my
+          organizational skills and time management as I was responsible for
+          delivering organized flyers and newspapers on time in my
+          neighbourhood.
+        </p>
+        <p
+          className="smaller-heading3"
+          style={{
+            color: theme === "light" ? "#000" : "#fff",
+          }}
+        >
+          If I am not studying or coding, I typically enjoy listening to
+          music, playing basketball, playing video games(find me on Valorant or
+          Fortnite!) or driving around.
+        </p>
+        <p
+          className="smaller-heading4"
+          style={{
+            color: theme === "light" ? "#000" : "#fff",
+          }}
+        >
+          I believe that I have the skills, passion, and work ethic to bring
+          positive contributions to the field of engineering and technology.
+        </p>
+      </div>
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import "./Navbar.css";
 import logo_light from "../assets/logo-black.png";
@@ -7,17 +7,37 @@ import dark_toggle from "../assets/day.png";
 import light_toggle from "../assets/night.png";
 
 const Navbar = ({ theme, setTheme }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const toggle_mode = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
   return (
-    <div className="navbar">
-      <img
-        src={theme === "light" ? logo_light : logo_dark}
-        alt=""
-        className="logo"
-      />
+    <div className={`Navbar ${isScrolled ? "blurred" : ""}`}>
+      <Link to="home" smooth={true} duration={500}>
+        <img
+          src={theme === "light" ? logo_light : logo_dark}
+          alt=""
+          className="logo"
+        />
+      </Link>
       <ul>
         <li>
           <Link to="home" smooth={true} duration={500}>
