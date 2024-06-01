6cea1ebae84e14bba37495c3faa6b4f7316bd517
jzliu <jzliu@uwaterloo.ca>
2024-06-01 00:05:44 -0400

    Changes to experiences section, added images

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Experiences.css";
+import resume from "../assets/JasonLiuResume.pdf";
+import alertdriving_logo from "../assets/alertdriving_logo.jpeg";
+import ontario_logo from "../assets/ontario.png";
+import chat_logo from "../assets/chat.png";
 
 const experiencesData = [
   {
@@ -7,17 +11,25 @@ const experiencesData = [
     title: "Software Quality Assurance Engineering Intern",
     company: "AlertDriving",
     duration: "May 2024 - Present",
-    description: "",
+    description: "Tools: Jira, PostgreSQL",
+    image: alertdriving_logo,
   },
   {
     id: 2,
     title: "IT QA Assistant",
     company: "Ontario Ministry of Education",
     duration: "Sept 2023 - Dec 2023",
-    description:
-      "Developed websites for clients using HTML, CSS, and JavaScript",
+    description: "Tools: HTML, CSS, JavaScript, Bootstrap",
+    image: ontario_logo,
   },
-  // Add more experiences as needed
+  {
+    id: 3,
+    title: "Data Processor",
+    company: "CHAT Insurance",
+    duration: "Jan 2023 - Apr 2023",
+    description: "Tools: Excel",
+    image: chat_logo,
+  },
 ];
 
 const Experiences = ({ theme }) => {
@@ -30,6 +42,11 @@ const Experiences = ({ theme }) => {
         <ul className="experiences-list">
           {experiencesData.map((experience) => (
             <li key={experience.id} className="experience-item">
+              <img
+                src={experience.image}
+                alt={experience.title}
+                className="experience-image"
+              />
               <h3>{experience.title}</h3>
               <p>{experience.company}</p>
               <p>{experience.duration}</p>
@@ -37,6 +54,14 @@ const Experiences = ({ theme }) => {
             </li>
           ))}
         </ul>
+        <a
+          href={resume}
+          className="resume-btn-experiences"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Full Resume {"->"}
+        </a>
       </div>
     </section>
   );
