3fd9c14a0481134204dff70c8d261224e9c4af6a
jzliu <jzliu@uwaterloo.ca>
2024-06-29 02:44:40 -0400

    Added dark images to projects sections

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,10 @@ import "./Projects.css";
 import { FaGithub } from "react-icons/fa";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import websitepic from "../assets/website.png";
+import darkwebsitepic from '../assets/darkwebsitepic.png';
+import javapic from "../assets/java.png"
+import darkjavapic from "../assets/javadark.png"
+import qm from "../assets/qm.png"
 
 const projectsdata = [
   {
@@ -13,71 +17,64 @@ const projectsdata = [
     githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
     tools: "React.js, CSS",
     image: websitepic, 
+    darkImage: darkwebsitepic,
   },
   {
     id: 2,
-    title: "Portfolio Website",
-    description: "My personal portfolio website created with React that highlights my work experience and projects.",
+    title: "Some Java Project",
+    description: "Some new Java project that I started working on.",
     link: "", 
-    githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
-    tools: "React.js, CSS",
-    image: websitepic, 
+    githublink: "", 
+    tools: "Java",
+    image: javapic, 
+    darkImage: darkjavapic,
   },
   {
     id: 3,
-    title: "Portfolio Website",
-    description: "My personal portfolio website created with React that highlights my work experience and projects.",
+    title: "To Be Thought Of",
+    description: "It doesn't exist yet. Please wait until I think of something.",
     link: "", 
-    githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
-    tools: "React.js, CSS",
-    image: websitepic, 
+    githublink: "", 
+    tools: "Insert some programming language", 
+    image: qm,
   },
   {
     id: 4,
-    title: "Portfolio Website",
-    description: "My personal portfolio website created with React that highlights my work experience and projects.",
+    title: "To Be Thought Of",
+    description: "It doesn't exist yet. Please wait until I think of something.",
     link: "", 
-    githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
-    tools: "React.js, CSS",
-    image: websitepic, 
+    githublink: "", 
+    tools: "Insert some programming language", 
+    image: qm,
   },
   {
     id: 5,
-    title: "Portfolio Website",
-    description: "My personal portfolio website created with React that highlights my work experience and projects.",
+    title: "To Be Thought Of",
+    description: "It doesn't exist yet. Please wait until I think of something.",
     link: "", 
-    githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
-    tools: "React.js, CSS",
-    image: websitepic, 
+    githublink: "", 
+    tools: "Insert some programming language", 
+    image: qm,
   },
   {
     id: 6,
-    title: "Portfolio Website",
-    description: "My personal portfolio website created with React that highlights my work experience and projects.",
+    title: "To Be Thought Of",
+    description: "It doesn't exist yet. Please wait until I think of something.",
     link: "", 
-    githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
-    tools: "React.js, CSS",
-    image: websitepic, 
-  },
-  {
-    id: 7,
-    title: "Portfolio Website",
-    description: "My personal portfolio website created with React that highlights my work experience and projects.",
-    link: "", 
-    githublink: "https://github.com/jasonliu1103/personalportfoliowebsite", 
-    tools: "React.js, CSS",
-    image: websitepic, 
+    githublink: "", 
+    tools: "Insert some programming language", 
+    image: qm,
   },
 ];
 
-const Projects = () => {
+const Projects = ({theme, setTheme}) => {
   return (
     <section id="projects">
       <div className="projects">
         <h1 className="title">Projects</h1>
         {projectsdata.map((project) => (
           <div key={project.id} className="project-card">
-            <img src={project.image} alt={project.title} className="project-image" />
+            <img src={theme === "dark" && project.darkImage ? project.darkImage : project.image} alt={project.title} className="project-image" />
             <h3 className="project-title">{project.title}</h3>
             <p className="project-description">{project.description}</p>
             <p className="project-tools">
